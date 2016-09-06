7cc25a3bfbc4016106dd14d29f6be7f8bc3f00a2
Gregory.Shrago <gregory.shrago@jetbrains.com>
2016-09-07 01:14:22 +0300

    formatter: align bindings & etc.

--- src/lang/clojure-formatter.kt
+++ src/lang/clojure-formatter.kt
@@ -85,6 +85,9 @@ data class Context(val common: CommonCodeStyleSettings,
                    val custom: ClojureCodeStyleSettings,
                    val spacingBuilder: SpacingBuilder) {
   val SHORT_ENOUGH = common.rootSettings.getRightMargin(ClojureLanguage) * 2 / 3
+
+  val newLineTop = Spacing.createSpacing(1, 1, 2, common.KEEP_LINE_BREAKS, common.KEEP_BLANK_LINES_IN_DECLARATIONS)!!
+  val newLine = Spacing.createSpacing(1, 1, 1, common.KEEP_LINE_BREAKS, common.KEEP_BLANK_LINES_IN_CODE)!!
 }
 
 class ClojureFormattingBlock(node: ASTNode,
@@ -101,14 +104,14 @@ class ClojureFormattingBlock(node: ASTNode,
   override fun isLeaf() = node.elementType !is IFileElementType && node.firstChildNode?.let {
     it == node.lastChildNode || node.elementType == C_SYMBOL && it.elementType is ClojureTokenType } ?: true
   override fun getIndent(): Indent =
-      if (sequenceIndex >= 0) Indent.getNormalIndent() else Indent.getNoneIndent()
+      if (sequenceIndex >= 0) Indent.getNormalIndent(true) else Indent.getNoneIndent()
 
   override fun getChildAttributes(newChildIndex: Int): ChildAttributes {
     val nodeType = node.elementType
     if (nodeType is IFileElementType) return ChildAttributes(Indent.getNoneIndent(), null)
     if (ClojureTokens.LIST_ALIKE.contains(nodeType)) {
       if (children.asSequence().take(newChildIndex).find { ClojureTokens.PARENS.contains(it.node.elementType) } != null) {
-        return ChildAttributes(Indent.getNormalIndent(), childAlignment)
+        return ChildAttributes(Indent.getNormalIndent(true), childAlignment)
       }
     }
     return ChildAttributes(Indent.getNoneIndent(), null)
@@ -120,19 +123,21 @@ class ClojureFormattingBlock(node: ASTNode,
     val psi = myNode.psi
     val psi1 = block1.node.psi!!
     val psi2 = child2.node.psi!!
-    if (psi1 is CDef && ClojureConstants.NS_ALIKE_SYMBOLS.contains(psi1.def.type)) {
-      return Spacing.createSpacing(1, 1, 2, context.common.KEEP_LINE_BREAKS, 10)
+    if (psi !is CForm && (psi1 is CDef || psi1 is CForm && psi2 is CDef)) {
+      return context.newLineTop
     }
-    val newLine = Spacing.createSpacing(1, 1, 1, context.common.KEEP_LINE_BREAKS, context.common.KEEP_BLANK_LINES_IN_CODE)
-    fun dependentSpacing(r: TextRange) = Spacing.createDependentLFSpacing(
+    val newLine = context.newLine
+    fun dependentSpacing(r: TextRange) = if (r.length >= context.SHORT_ENOUGH && psi2 is CForm) newLine
+    else Spacing.createDependentLFSpacing(
         0, 1, r, context.common.KEEP_LINE_BREAKS, context.common.KEEP_BLANK_LINES_IN_CODE)
     fun dependentSpacing1() = dependentSpacing(TextRange.create(textRange.startOffset, block2.textRange.startOffset))
     fun dependentSpacing2() = dependentSpacing(TextRange.create(block1.textRange.startOffset,
-        if (block2.node.psi is CSForm) block1.textRange.endOffset else block2.textRange.endOffset))
+        if (psi2 is CSForm) block1.textRange.endOffset else block2.textRange.endOffset))
+    fun dependentSpacing3() = dependentSpacing(TextRange.create(block1.textRange.startOffset, block2.textRange.endOffset))
     if (psi is CSymbol) {
       if (psi1 is CMetadata) return dependentSpacing1()
     }
-    if (block2.sequenceIndex >= 0 && !ClojureTokens.PARENS.contains(child2.node.elementType)) {
+    if (block2.sequenceIndex > 0 && !ClojureTokens.PARENS.contains(child2.node.elementType)) {
       when (psi) {
         is CMap -> {
           return if (block2.sequenceIndex > 0 && block2.sequenceIndex % 2 == 0) newLine
@@ -140,14 +145,10 @@ class ClojureFormattingBlock(node: ASTNode,
         }
         is CDef -> when {
           psi.def.type == "ns" -> if (block2.sequenceIndex > 1) return newLine
-          psi.def.type.startsWith("def") -> (children.find { it.node.psi == psi.nameSymbol }?.sequenceIndex ?: 1).let {
-            return if (block2.sequenceIndex > it) {
-              if (block2.textRange.length < context.SHORT_ENOUGH &&
-                  block2.sequenceIndex == children[children.size - 2].sequenceIndex) null
-              else newLine
-            }
-            else dependentSpacing2()
-          }
+          psi.def.type.startsWith("def") -> return if (psi2 is CVec) null
+          else if (block2.textRange.length < context.SHORT_ENOUGH) dependentSpacing3()
+          else newLine
+          else -> return newLine
         }
         is CVec -> {
           val parent = psi.parent.run { if (findChild(CForm::class) == psi) parent else this }
@@ -155,7 +156,7 @@ class ClojureFormattingBlock(node: ASTNode,
           else (parent as? CList)?.first?.name.let {
             if (it != null && ClojureConstants.FN_ALIKE_SYMBOLS.contains(it)) return null
             else if (it != null && ClojureConstants.LET_ALIKE_SYMBOLS.contains(it) && parent == psi.parent) {
-              return if (block2.sequenceIndex > 0 && block2.sequenceIndex % 2 == 0) newLine else dependentSpacing2()
+              return if (block2.sequenceIndex > 0 && block2.sequenceIndex % 2 == 0) newLine else null
             }
           }
         }
@@ -173,7 +174,8 @@ class ClojureFormattingBlock(node: ASTNode,
           else if (type == "cond->" || type == "cond->>") {
             if (block2.sequenceIndex > 2 && block2.sequenceIndex % 2 == 0) newLine else dependentSpacing2()
           }
-          else null
+          else if (ClojureConstants.LET_ALIKE_SYMBOLS.contains(type) && psi2 is CVec) null
+          else dependentSpacing2()
         }
       }
       return dependentSpacing1()
@@ -189,14 +191,15 @@ class ClojureFormattingBlock(node: ASTNode,
     var prevIndex = -1
     val nextIndex: ()->Int = if (!ClojureTokens.LIST_ALIKE.contains(nodeType)) {{ -1 }} else {{ if (!leftParen) -1 else ++prevIndex }}
 
+    val psi = node.psi
     val minAlignIndex = if (nodeType != C_LIST || node.firstChildNode.elementType == C_READER_MACRO) 0
-    else if (node.psi is CDef || node.psi.parents().skip(1).filter(CForm::class).isEmpty) Integer.MAX_VALUE
-    else if ((node.psi as? CList)?.first?.name?.let {
+    else if (psi is CDef || psi.parents().skip(1).filter(CForm::class).isEmpty) Integer.MAX_VALUE
+    else if ((psi as? CList)?.first?.name?.let {
       ClojureConstants.FN_ALIKE_SYMBOLS.contains(it) || ClojureConstants.LET_ALIKE_SYMBOLS.contains(it) } ?: false) Integer.MAX_VALUE
     else 1
 
     val metaAlign = if (node.firstChildNode?.elementType == C_METADATA) Alignment.createAlignment() else null
-    val mapAlign = if (node.psi is CMap) Alignment.createAlignment(true) else null
+    val mapAlign = if (psi is CMap || psi is CVec && (psi.parent as? CList)?.first?.name.elementOf(ClojureConstants.LET_ALIKE_SYMBOLS)) Alignment.createAlignment(true) else null
     return node.iterate().transform(Function<ASTNode, ClojureFormattingBlock?> f@ {
       val type = it.elementType
       if (type == TokenType.WHITE_SPACE && it.textLength == 1 && it.text == "," ||
